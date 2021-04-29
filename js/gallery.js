a52f6b21c03efd72ed20f5611e4748bd737836dd
Ржанов Володимир <emitter@10a>
2021-04-29 17:56:13 +0300

    добавляет больший картинки

--- js/gallery.js
+++ js/gallery.js
@@ -6,24 +6,46 @@ const refs = {
     btnLightboxClose: document.querySelector('[data-action="close-lightbox"]'),
     lightboxOverlay: document.querySelector('.lightbox__overlay'),
     lightBoxImage: document.querySelector('.lightbox__image'),
-    // gallery: document.querySelector(),
 };
 
 const outputGallery = galleryItemsPreview => {
     
-    for (const item of galleryItemsPreview) {
+    // for (const item of galleryItemsPreview) {
+    //     const makeGallery = () =>
+    //         [`
+    //     <li class="gallery__item">
+    //     <a
+    //         class="gallery__link"
+    //         href="${item.original}"
+    //     >
+    //         <img
+    //         class="gallery__image"
+    //         src="${item.preview}"
+    //         data-source="${item.original}"
+    //         alt="${item.description}"
+    //         data-scrolling = ""
+    //         />
+    //     </a>
+    //     </li>
+    //     `].join('');
+
+    //     refs.gallery.insertAdjacentHTML('beforeend', makeGallery());
+    // };
+
+    for (let i = 0; i < galleryItemsPreview.length; i +=1) {
         const makeGallery = () =>
             [`
         <li class="gallery__item">
         <a
             class="gallery__link"
-            href="${item.original}"
+            href="${galleryItemsPreview[i].original}"
         >
             <img
             class="gallery__image"
-            src="${item.preview}"
-            data-source="${item.original}"
-            alt="${item.description}"
+            src="${galleryItemsPreview[i].preview}"
+            data-source="${galleryItemsPreview[i].original}"
+            alt="${galleryItemsPreview[i].description}"
+            data-index = "${[i]}"
             />
         </a>
         </li>
@@ -38,29 +60,45 @@ const openModalWindow = (e) => {
     if (!e.target.classList.contains('gallery__image')) {
         return;
     }
+    refs.btnLightboxClose.addEventListener('click', onCloseModalWindow);
+    refs.lightboxOverlay.addEventListener('click', onCloseModalWindow);
+    window.addEventListener('keydown', clickModalByKey);
+    window.addEventListener('keydown', onKeyChangeImage);
     e.preventDefault();
     refs.lightbox.classList.add('is-open');
-    // console.log(refs.lightBoxImage.classList.lightbox__image.value  = `${item.original}`)
+    refs.lightBoxImage.src = e.target.dataset.source;
+    refs.lightBoxImage.alt = e.target.alt;
+    refs.lightBoxImage.dataset.scrolling = e.target.dataset.index;
 };
 
-const onCloseModalWindow = () => {
+const onCloseModalWindow = e => {
     refs.lightbox.classList.remove('is-open');
     if (e.key === 'Escape') {
         refs.lightbox.classList.remove('is-open');
     };
-};
-
-const onLightboxCloseModalWindow = () => {
-        refs.lightbox.classList.remove('is-open');
+    refs.lightBoxImage.src = '';
+    refs.lightBoxImage.alt = '';
+    refs.btnLightboxClose.removeEventListener('click', onCloseModalWindow);
+    refs.lightboxOverlay.removeEventListener('click', onCloseModalWindow);
+    window.removeEventListener('keydown', clickModalByKey);
+    window.removeEventListener('keydown', onKeyChangeImage);
 };
 
 const clickModalByKey = e => {
     if (e.key === 'Escape') {
-        refs.lightbox.classList.remove('is-open');
+        onCloseModalWindow(e);
+    };
+};
+
+const onKeyChangeImage = e => {
+    if (e.key === 'ArrowLeft') {
+        console.log(e.key);
+        console.log(refs.lightBoxImage.dataset.scrolling)
+    }
+    else if (e.key === 'ArrowRight') {
+        console.log(e.key);
+        console.log(refs.lightBoxImage.dataset.scrolling)
     };
 };
 
 refs.gallery.addEventListener('click', openModalWindow);
-refs.btnLightboxClose.addEventListener('click', onCloseModalWindow);
-refs.lightboxOverlay.addEventListener('click', onLightboxCloseModalWindow);
-window.addEventListener('keydown', clickModalByKey);

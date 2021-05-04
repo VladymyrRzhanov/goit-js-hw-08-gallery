4e806ccb949d704ab72cfe42309028c8973517cc
Vladymyr Rzhanov <v.rzhanov@gmail.com>
2021-05-04 17:37:03 +0300

    добавляет круговую прокрутку

--- js/gallery.js
+++ js/gallery.js
@@ -71,16 +71,35 @@ let index = 0;
 
 const onLeftKeyChangeImage = e => {
     index = Number(refs.lightBoxImage.dataset.index);
+            console.log(index);
+
     if (e.code === 'ArrowLeft') {
-        index -= 1;
-        changedImageByKey();
+            console.log(index);
+
+        if (index <= 0) {
+            index += galleryItems.length-1;
+            console.log(index);
+            changedImageByKey();
+        }
+        else {
+            index -= 1;
+            console.log(index);
+            changedImageByKey();
+        };
     };
 };
 
 const onRightKeyChangeImage = e => {
     if (e.code === 'ArrowRight') {
-        index += 1;
-        changedImageByKey();
+        if (index >= galleryItems.length-1) {
+            index -= index;
+            console.log(index);
+            changedImageByKey();
+        } else {
+            index += 1;
+            console.log(index);
+            changedImageByKey();
+        }
     };
 };
 
@@ -91,85 +110,3 @@ const changedImageByKey = () => {
 };
 
 refs.gallery.addEventListener('click', openModalWindow);
-
-
-
-
-
-
-
-
-
-//     lightBoxImage: document.querySelector('.lightbox__image'),
-// };
-
-// const outputGallery = galleryItemsPreview => {
-//    const makeGallery = galleryItemsPreview.map((item,index) => `<li class="gallery__item">
-//        <a class="gallery__link" href="${item.original}">
-//        <img class="gallery__image" src="${item.preview}"
-//        data-source="${item.original}"
-//        alt="${item.description}"
-//        data-index="${index}"/>
-//        </a>
-//        </li>`).join('');
-//     refs.gallery.insertAdjacentHTML('beforeend', makeGallery);
-    
-// };
-
-
-// outputGallery(galleryItems);
-
-// const openModalWindow = (e) => {
-//     e.preventDefault();
-
-//     if (!e.target.classList.contains('gallery__image')) {
-//         return;
-//     }
-//     refs.lightbox.classList.add('is-open');
-//     refs.lightBoxImage.src = e.target.dataset.source;
-//     refs.lightBoxImage.alt = e.target.alt;
-//     refs.lightBoxImage.dataset.index = Number(e.target.dataset.index);
-//     refs.btnLightboxClose.addEventListener('click', onCloseModalWindow);
-//     refs.lightbox.addEventListener('click', onCloseModalByClick);
-//     window.addEventListener('keydown', clickModalByKey);
-//     window.addEventListener('keydown', onKeyChangeImage);
-// };
-
-// const onCloseModalByClick = e => {
-//     console.log(e.target)
-//     console.log(e.currentTarget)
-//     if (e.target === e.currentTarget) {
-//     // onCloseModalWindow()
-// }
-// }
-
-// const onCloseModalWindow = e => {
-//     refs.lightbox.classList.remove('is-open');
-    
-//     refs.lightBoxImage.src = '';
-//     refs.lightBoxImage.alt = '';
-//     refs.btnLightboxClose.removeEventListener('click', onCloseModalWindow);
-//     refs.lightboxOverlay.removeEventListener('click', onCloseModalByClick);
-//     window.removeEventListener('keydown', clickModalByKey);
-//     window.removeEventListener('keydown', onKeyChangeImage);
-// };
-
-// const clickModalByKey = e => {
-//     if (e.code === 'Escape') {
-//         onCloseModalWindow(e);
-//     };
-// };
-
-// const onKeyChangeImage = e => {
-//     if (e.code === 'ArrowLeft') {
-
-//         // console.log(e.code);
-//         // console.log(refs.lightBoxImage.dataset.scrolling)
-//     }
-//     else if (e.code === 'ArrowRight') {
-//         console.log(e.code);
-//         console.log(refs.lightBoxImage.dataset.scrolling)
-//     };
-// };
-
-// refs.gallery.addEventListener('click', openModalWindow);
